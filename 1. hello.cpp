71d4067ed63a50f66b22b23656b61c5c542919d1
Jay Chotaliya <jaychotaliya987@gmail.com>
2023-05-24 16:22:19 +0200

    Add files via upload

---
+++ 1. hello.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+/*Include means import in python but some 
+libraries in python are not to be imported called
+standerd libraries but you have to import it in C++
+bummer*/
+
+int main()
+{ 
+   std::cout << "hello world" << std::endl;
+
+   /*std = standerd library
+   cout = output command similar to print function
+   we have to inclde std before using standerd 
+   libraries in C++
+   endl = end line
+   */
+   
+   return 0;
+}
