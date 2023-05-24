9af7394bb4855d36fbc18049a2bfad444b34660a
Jay Chotaliya <jaychotaliya987@gmail.com>
2023-05-24 16:28:25 +0200

    Add files via upload

---
+++ 2. Arithematic operations/1.power.cpp
@@ -0,0 +1,8 @@
+#include <cmath>
+#include <iostream>
+
+int main()
+{
+    std::cout<< std::pow(3,2);
+    return 0;
+}
---
+++ 2. Arithematic operations/2. Area,Vol.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    //Dimension of the cube
+    float cubeSide = 5.4;
+    //Dimension of sphere
+    float sphereRadius = 2.33;
+    //Dimensions of cone
+    float coneRadius = 7.65;
+    float coneHeight = 14;
+    
+    float volCube, volSphere, volCone = 0;
+    
+    volCube = pow(cubeSide,3);
+    volSphere = (4/3)* 3.14 * (pow(sphereRadius,3));
+    volCone = M_1_PI * (pow(coneRadius,2)) * (coneHeight/3);
+
+    cout << "volume of cube is " << volCube << endl;
+    cout << "volume of sphere is " << volSphere << endl;
+    cout << "volume of cone is " << volCone << endl ;
+    
+    return 0;
+}
