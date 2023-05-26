eb2855c82e492031a41f9606571d7cc05337e00d
Jay Chotaliya <jaychotaliya987@gmail.com>
2023-05-26 15:56:18 +0200

    Update 1.RelationalOP.cpp

--- 3.Control_Flow/1.RelationalOP.cpp
+++ 3.Control_Flow/1.RelationalOP.cpp
@@ -18,4 +18,18 @@ int main()
     std::cout<<"\nc >= b is "<<TorF[c>=b];
     std::cout<<"\nc <= b is "<<TorF[c<=b];    
     return 0;
+       
+    
+    // We can also check if one value is equal to other and if they are
+    // equal print 1(true) otherwise 0(false)
+
+    std::cout << "\n a = b is  " << TorF[!(a != b)];
+    // !before the operation acts as the not get and invert the value so 
+    //this is now acting as the equality check. before it was running not equal check.
+    
+    std::cout << "\n " << TorF[a==b];
+    // This is also possible but above method will also work if you explicitly want to
+    // print 0 or 1
+    return 0;
 }
+
