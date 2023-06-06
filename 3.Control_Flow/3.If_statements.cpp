006fa168069d70d16f7ebaf7cc1ab33b84131036
Jay Chotaliya <jaychotaliya987@gmail.com>
2023-06-06 16:54:13 +0200

    Add files via upload

---
+++ 3.Control_Flow/3.If_statements.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main ()
+{
+    int a = 10;
+
+    if (a == 10)
+    {
+        std::cout << "a is equal to 10";
+    }
+
+    return 0;
+}
---
+++ 3.Control_Flow/4.Ifelse_statement.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main ()
+{
+    int a = 11;
+
+    if (a == 10)
+    {
+        std::cout << "a is equal to 10";
+    }
+    else
+    {
+        std::cout << "a is not equal to 10";
+    }
+
+
+    return 0;
+}
